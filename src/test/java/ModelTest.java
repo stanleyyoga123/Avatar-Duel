69be86c5e4105d45d69d6e5cbca7fc50951ead5c
bambangharyopba <bambanganggit@gmail.com>
2020-04-19 21:58:31 +0700

    add skill tester and player tester

--- src/test/java/ModelTest.java
+++ src/test/java/ModelTest.java
@@ -1,7 +1,11 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import com.avatarduel.model.attribute.Attribute;
-import com.avatarduel.model.type.Element;
+import java.util.ArrayList;
+
+import com.avatarduel.model.attribute.*;
+import com.avatarduel.model.type.*;
+import com.avatarduel.model.Card;
 import com.avatarduel.model.Character;
 import com.avatarduel.model.Land;
 import com.avatarduel.model.Skill;
@@ -14,6 +18,8 @@ public class ModelTest {
     public void characterTester(){
         Attribute attribute = new Attribute(0,1,2);
         Character test = new Character("Test", Element.WATER, "test description", attribute);
+
+        assertTrue(test instanceof Card);
 
         assertEquals(test.getName(), "Test");
         assertEquals(test.getElement(), Element.WATER);
@@ -31,9 +37,12 @@ public class ModelTest {
         assertEquals(test.getAttribute().getPower(), 0);
     }
 
+    @Test
     public void landTester(){
         Attribute attribute = new Attribute(0,1,2);
         Land test = new Land("Test", Element.FIRE, "test description");
+
+        assertTrue(test instanceof Card);
 
         assertEquals(test.getName(), "Test");
         assertEquals(test.getElement(), Element.FIRE);
@@ -48,5 +57,41 @@ public class ModelTest {
         assertEquals(test.getAttribute().getAttack(), 0);
         assertEquals(test.getAttribute().getDefense(), 0);
         assertEquals(test.getAttribute().getPower(), 0);
+    }
+
+    @Test
+    public void skillTester(){
+        Attribute attribute = new Attribute(0,0,0);
+        Skill s = new Skill("Skill Card", Element.WATER, "Skill Desc", Effect.POWER_UP, attribute);
+
+        assertTrue(s instanceof Card);
+        
+        assertEquals(s.getName(), "Skill Card");
+        assertEquals(s.getElement(), Element.WATER);
+        assertEquals(s.getDescription(), "Skill Desc");
+        assertEquals(s.getEffect(), Effect.POWER_UP);
+        assertEquals(s.getAttribute().getAttack(), 0);
+        assertEquals(s.getAttribute().getDefense(), 0);
+        assertEquals(s.getAttribute().getPower(), 0);
+
+        attribute = new Attribute(1,2,3);
+        s.setAttribute(attribute);
+
+        assertEquals(s.getAttribute().getAttack(), 1);
+        assertEquals(s.getAttribute().getDefense(), 2);
+        assertEquals(s.getAttribute().getPower(), 3);
+    }
+
+    @Test
+    public void playerTest(){
+        Player p = new Player(new ArrayList<Card>(), 10, false, new MidDeck(new ArrayList<Card>(), new ArrayList<Card>()), new Deck(), new Power(0,0,0,0,0), new RemainingPower(0,0,0,0,0));
+        
+        assertTrue(p.getHandDeck() instanceof ArrayList);
+        assertEquals(p.getHealth(), 10);
+        assertEquals(p.isPlayedLand(), false);
+        assertTrue(p.getMidDeck() instanceof MidDeck);
+        assertTrue(p.getDrawDeck() instanceof Deck);
+        assertTrue(p.getPower() instanceof Power);
+        assertTrue(p.getRemPower() instanceof RemainingPower);
     }
 }
